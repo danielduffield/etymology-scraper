a134029004543fac14cfe790570513217b385702
Daniel Duffield <danieljduffield@gmail.com>
2017-11-18 14:47:06 -0800

    Created a handler for receiving query results from URL submission. #10

--- client/utils/store.js
+++ client/utils/store.js
@@ -1,14 +1,23 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers } from "redux";
 
-function reducer(state = {
-  urlInputValue: ''
-}, action) {
+function reducer(
+  state = {
+    urlInputValue: "",
+    results: ""
+  },
+  action
+) {
   switch (action.type) {
-    case 'UPDATED_URL_INPUT_VALUE':
-      return Object.assign({}, state, { urlInputValue: action.payload.text })
+    case "UPDATED_URL_INPUT_VALUE":
+      return Object.assign({}, state, { urlInputValue: action.payload.text });
+    case "RECEIVED_QUERY_RESULTS":
+      return Object.assign({}, state, {
+        urlInputValue: "",
+        results: action.payload.response
+      });
     default:
-      return state
+      return state;
   }
 }
 
-export default createStore(reducer)
+export default createStore(reducer);
