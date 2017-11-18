b315f28fa58f158ed2c488a1d3c87b457bbe0d79
Audiodrome <lien.daniel.h@gmail.com>
2017-11-18 14:59:23 -0800

    Init redis db

--- server/controllers/scrapeController.js
+++ server/controllers/scrapeController.js
@@ -1,6 +1,24 @@
 const Scraper = require("../lib/scraper");
 const scraper = new Scraper();
 
+const redis = require("redis");
+const client = redis.createClient();
+
+client.on("error", (err) => {
+  console.log("Error ", err);
+});
+
+// client.hmset('frameworks', 'javascript', 'AngularJS', 'css', 'Bootstrap', 'node', 'Express');
+//
+// client.hgetall('frameworks', function(err, object) {
+//     console.log(object);
+// });
+
+// client.set("string key", "string val", redis.print);
+// client.get("string key", (err, object) => {
+//   console.log(object);
+// });
+
 class ScrapeController {
   scrapeUrl(req, res) {
     const body = req.body;
