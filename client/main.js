f1cc1411fb5c6906d671934ca66192d4bc867419
Daniel Duffield <danieljduffield@gmail.com>
2017-11-18 12:59:06 -0800

    Imported the redux store and Provider. #4

--- client/main.js
+++ client/main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './utils/store'
 
 import App from './utils/app.js'
 
@@ -9,7 +11,9 @@ render()
 
 function render() {
   ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     $root
   )
 }
