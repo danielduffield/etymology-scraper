26f3b3c24f6ec234e3b87881f6b15a826cfe85c8
Daniel Duffield <danieljduffield@gmail.com>
2017-11-18 13:45:41 -0800

    Created methods for the urlSubmitForm to handle input value change and form submission. #4

--- client/utils/urlSubmitForm.js
+++ client/utils/urlSubmitForm.js
@@ -1,20 +1,63 @@
 import React from 'react'
 import styled from 'styled-components'
+import { connect } from 'react-redux'
 
-export default class UrlSubmitForm extends React.Component {
+class UrlSubmitForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.updateUrlInput = this.updateUrlInput.bind(this)
+    this.submitUrl = this.submitUrl.bind(this)
+
+    this.urlForm = null
+  }
+  updateUrlInput(event) {
+    this.props.dispatch({
+      type: 'UPDATED_URL_INPUT_VALUE',
+      payload: { text: event.target.value }
+    })
+  }
+  submitUrl(event) {
+    event.preventDefault()
+    const url = new FormData(this.urlForm).get('url-submit-input')
+    fetch('api/url', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({url})
+    }).then(res => console.log(res))
+  }
   render() {
     return (
       <div className="container-fluid">
-        <StyledRow className="row">
+        <div className="row">
           <div className="col-6 offset-3">
-            <p>URL SUBMIT FORM</p>
+            <form id="url-submit-form" onSubmit={this.submitUrl}
+              ref={form => {
+                this.urlForm = form
+              }}>
+              <div className="input-group">
+                <input type="text" id="url-submit-input" name="url-submit-input" className="form-control" placeholder="Your URL Goes Here"
+                  onChange={this.updateUrlInput} value={this.props.urlInputValue}/>
+                <span className="input-group-btn">
+                  <button id="url-submit-btn" className="btn btn-default" type="submit">Engage!</button>
+                </span>
+              </div>
+            </form>
           </div>
-        </StyledRow>
+        </div>
       </div>
     )
   }
 }
 
-const StyledRow = styled.div`
-  background-color: yellow;
-`
+function mapStateToProps(state) {
+  return {
+    urlInputValue: state.urlInputValue,
+  }
+}
+
+const Connected = connect(mapStateToProps)(UrlSubmitForm)
+export default Connected
