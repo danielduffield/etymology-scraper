705757324725fac29344372e434c0778c6b44ede
Daniel Duffield <danieljduffield@gmail.com>
2017-11-18 12:10:43 -0800

    Imported urlSubmitForm react component and rendered at . #1

--- client/main.js
+++ client/main.js
@@ -1,1 +1,15 @@
-console.log('Testing')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import UrlSubmitForm from './utils/urlSubmitForm.js'
+
+const $root = document.getElementById('root')
+
+render()
+
+function render() {
+  ReactDOM.render(
+    <UrlSubmitForm />,
+    $root
+  )
+}
