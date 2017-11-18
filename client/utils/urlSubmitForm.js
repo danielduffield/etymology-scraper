ae505a0f69ed3c4456fa927d894da27e2b0c59fc
Daniel Duffield <danieljduffield@gmail.com>
2017-11-18 12:51:30 -0800

    Imported styled components and used Bootstrap column layout. #4

--- client/utils/urlSubmitForm.js
+++ client/utils/urlSubmitForm.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default class UrlSubmitForm extends React.Component {
   render() {
     return (
-      <div>
-        <p>URL SUBMIT FORM</p>
+      <div className="container-fluid">
+        <StyledRow className="row">
+          <div className="col-6 offset-3">
+            <p>URL SUBMIT FORM</p>
+          </div>
+        </StyledRow>
       </div>
     )
   }
 }
+
+const StyledRow = styled.div`
+  background-color: yellow;
+`
