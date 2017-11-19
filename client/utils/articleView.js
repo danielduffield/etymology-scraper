4c2f7f7153301c56fcd4f87c28874ba050535854
Daniel Duffield <danieljduffield@gmail.com>
2017-11-19 01:05:03 -0800

    Refactored articleView method of scanning and highlighting keywords. #13

--- client/utils/articleView.js
+++ client/utils/articleView.js
@@ -4,53 +4,45 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 class ArticleView extends React.Component {
+  scanContents(text, keywords) {
+    let acc = "";
+    let toRender = [];
+    text.split(" ").forEach((word, i) => {
+      let matched = false;
+      keywords.forEach((match, index) => {
+        if (match.toLowerCase() === word.toLowerCase()) {
+          toRender.push(<span>{acc}</span>);
+          toRender.push(
+            <KeywordMatch className="keyword" matchIndex={index}>
+              {word}
+            </KeywordMatch>
+          );
+          acc = " ";
+          matched = true;
+        }
+      });
+      acc = matched ? " " : acc + word + " ";
+      matched = false;
+    });
+    return toRender;
+  }
   render() {
     return (
       <div>
         <h3>
           <a href={this.props.results.url}>{this.props.results.title}</a>
         </h3>
-        {this.props.results.contents
-          ? flagMatches(
+        {this.props.results.etymologies
+          ? this.scanContents(
               this.props.results.contents,
               this.props.results.etymologies.map(
                 wordData => wordData.word.normal
               )
             )
-              .split("<<<")
-              .map(line => line.split(">>>"))
-              .map((parsed, index) => {
-                if (index === 0) {
-                  const temp = parsed.slice()[0];
-                  parsed[0] = parsed[1];
-                  parsed[1] = temp;
-                }
-                const matchIndex = this.props.results.etymologies
-                  .map(wordData => wordData.word.normal)
-                  .findIndex(word => word === parsed[0]);
-                console.log(matchIndex);
-                return (
-                  <span key={index}>
-                    <KeywordMatch className="keyword" matchIndex={matchIndex}>
-                      {parsed[0]}
-                    </KeywordMatch>
-                    <span>{parsed[1]}</span>
-                  </span>
-                );
-              })
           : ""}
       </div>
     );
   }
-}
-
-function flagMatches(text, keywords) {
-  let flagged = text;
-  keywords.forEach(word => {
-    const regexMatch = new RegExp("[^a-zA-Zd]" + word + "[^a-zA-Zd]", "gi");
-    flagged = flagged.replace(regexMatch, " <<<" + word + ">>> ");
-  });
-  return flagged;
 }
 
 ArticleView.propTypes = {
