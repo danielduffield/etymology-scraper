e0f6a43d95315d6d2560da12d8fc990ef8d45f05
Audiodrome <lien.daniel.h@gmail.com>
2017-11-19 14:52:12 -0800

    Fix promises with async

--- server/controllers/scrapeController.js
+++ server/controllers/scrapeController.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const Scraper = require("../lib/scraper");
 const scraper = new Scraper();
 
@@ -5,7 +6,33 @@ const Word = require("../lib/word");
 const word = new Word();
 
 // word.isWordInRedis('string key');
-// word.saveWordInRedis('science');
+
+// word.setWordInRedis('astronomy');
+// word.setWordInRedis('science');
+// word.setWordInRedis('literature');
+
+// const wordArr = ['astronomy', 'literature', 'acapella', 'arithmetic'];
+const wordArr = ['acapella', 'acapella', 'macaronic'];
+// word.getWordInRedis('astronomy');
+// const redis_word = word.getWordInRedis('literature');
+// console.log('redis word', redis_word);
+
+// Promise.all(wordArr.map(item => word.isWordInRedis(item)))
+//   .then(() => {
+//     Promise.all(wordArr.map(item => word.getWordInRedis(item)))
+//       .then(data => {
+//         console.log('array docs', util.inspect(data, { showHidden: true, depth: null }));
+//       });
+//   });
+
+Promise.all(wordArr.map(item => word.getWordInRedis(item)
+  .then(result => result
+    ? Promise.resolve(result)
+    : word.setWordInRedis(item))
+  ))
+  .then(data => {
+    console.log('array docs', util.inspect(data, { showHidden: true, depth: null }));
+  });
 
 class ScrapeController {
   scrapeUrl(req, res) {
--- server/lib/word.js
+++ server/lib/word.js
@@ -4,6 +4,11 @@ const axios = require("axios");
 const parser = require('xml2json');
 const util = require('util');
 const parseString = require('xml2js').parseString;
+const bluebird = require("bluebird");
+const Promise = require('bluebird');
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 
 client.on("error", (err) => {
   console.log("Error ", err);
@@ -23,61 +28,83 @@ client.set("string key", "string val", redis.print);
 // });
 
 class Word {
-  isWordInRedis(word) {
-    console.log("check word is in redis");
+  // isWordInRedis(word) {
+  //   return client.getAsync(word)
+  //     .then(data => {
+  //       if (data === null) {
+  //         // console.log('did i make it', word);
+  //         return this.setWordInRedis(word);
+  //       }
+  //     });
+  // }
+
+  getWordInRedis(word) {
     // check to see if word is in redis
-    client.get(word, (err, object) => {
-      if (err) {
-        return err;
-      }
-      return object;
-    });
+    console.log("check word is in redis", word);
+    return client.getAsync(word)
+      .then(data => {
+        console.log('this word in redis', data);
+        return JSON.parse(data);
+      });
   }
 
-  saveWordInRedis(word) {
-    console.log("save word in redis");
+  setWordInRedis(word) {
+    console.log('saving ', word);
+    // make a request to merriam webster
+    // save word in redis
+    // console.log("save word in redis");
     const url =
       `https://www.dictionaryapi.com/api/v1/references/collegiate/xml/${word}?key=${dictionary_api}`;
 
-    axios
+     return axios
       .get(url)
       .then(response => {
-        parseString(response.data, function (err, result) {
-          // console.log(util.inspect(result.entry_list.entry[0].def[0].date[0], { showHidden: true, depth: null }));
-          const document = {};
-          const etm = [];
-          let word = result.entry_list.entry[0].$.id;
-          let date = result.entry_list.entry[0].def[0].date[0];
-          let key = result.entry_list.entry[0].et[0]._;
-          let value = result.entry_list.entry[0].et[0].it;
-          key = key.split('  ');
-          // console.log('key', key);
-          // console.log('value', value);
-          for (let i = 0; i < key.length; i++) {
-            if (value[i] === undefined) {
-              let obj = {};
-              obj[key[i]] = null;
-              etm.push(obj);
-            } else {
-              let obj = {};
-              obj[key[i]] = value[i];
-              etm.push(obj);
-            }
-          }
-
-          document['etm'] = etm;
-          document['word'] = word;
-          document['date'] = date;
-          console.log('document to save', document);
-          client.set(word, JSON.stringify(document), redis.print);
-        });
+        return parseStringAsync(response.data);
       })
       .catch(error => {
         console.log(error);
       });
-    // make a request to merriam webster
-    // save word in redis
   }
+}
+
+const parseStringAsync = function(data) {
+  const promise = new Promise((resolve, reject) => {
+    parseString(data, function (err, result) {
+      // console.log(util.inspect(result.entry_list.entry[0].def[0].date[0], { showHidden: true, depth: null }));
+      if (err) {
+        reject(err);
+      } else {
+        const document = {};
+        const etm = [];
+        let word = result.entry_list.entry[0].$.id;
+        let date = result.entry_list.entry[0].def[0].date[0];
+        let key = result.entry_list.entry[0].et[0]._;
+        let value = result.entry_list.entry[0].et[0].it;
+        key = key.split('  ');
+
+        for (let i = 0; i < key.length; i++) {
+          if (value[i] === undefined) {
+            let obj = {};
+            obj[key[i]] = null;
+            etm.push(obj);
+          } else {
+            let obj = {};
+            obj[key[i]] = value[i];
+            etm.push(obj);
+          }
+        }
+
+        document['etm'] = etm;
+        document['word'] = word;
+        document['date'] = date;
+        console.log('document to save', document);
+        client.set(word, JSON.stringify(document), redis.print);
+        // return document;
+        resolve(document);
+      }
+    });
+  });
+  return promise;
 }
 
 module.exports = Word;
