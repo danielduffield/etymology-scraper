04ece0f213ce7e5855f5d6f98276f92020ef03e3
kk99807 <kk99807@gmail.com>
2017-11-18 22:42:01 -0800

    Added example barchart code

---
+++ example_charts/data.js
@@ -0,0 +1,200 @@
+var etymologies = [
+	{
+		"word": {
+			"normal": "Karen",
+			"count": 255,
+			"percent": 25
+		},
+		"origin": "Old English",
+		"etymology": "fact etymology obj"
+	},
+	{
+		"word": {
+			"normal": "CJ",
+			"count": 390,
+			"percent": 50
+		},
+		"origin": "Old English",
+		"etymology": "fact etymology obj"
+	},
+    {
+      "word": {
+        "normal": "abortion",
+        "count": 8,
+        "percent": 4.35
+      },
+	  "origin": "Old English",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "jones",
+        "count": 7,
+        "percent": 3.8
+      },
+		"origin": "Old English",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "life",
+        "count": 5,
+        "percent": 2.72
+      },
+	  "origin": "Old English",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "doug jones",
+        "count": 4,
+        "percent": 2.17
+      },
+		"origin": "Old English",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "alabama",
+        "count": 4,
+        "percent": 2.17
+      },
+		"origin": "Old English",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "term abortions",
+        "count": 4,
+        "percent": 2.17
+      },
+	  "origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "baby",
+        "count": 3,
+        "percent": 1.63
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "people",
+        "count": 3,
+        "percent": 1.63
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "roy moore",
+        "count": 3,
+        "percent": 1.63
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "comments",
+        "count": 3,
+        "percent": 1.63
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "access",
+        "count": 3,
+        "percent": 1.63
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "his",
+        "count": 3,
+        "percent": 1.63
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "womb",
+        "count": 3,
+        "percent": 1.63
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "election",
+        "count": 2,
+        "percent": 1.09
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "unsure",
+        "count": 2,
+        "percent": 1.09
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "breitbart news",
+        "count": 2,
+        "percent": 1.09
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "decision",
+        "count": 2,
+        "percent": 1.09
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "position",
+        "count": 2,
+        "percent": 1.09
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "planned parenthood",
+        "count": 2,
+        "percent": 1.09
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    },
+    {
+      "word": {
+        "normal": "term",
+        "count": 1,
+        "percent": 0.54
+      },
+		"origin": "French",
+      "etymology": "fact etymology obj"
+    }
+];
---
+++ example_charts/options.barchart.js
@@ -0,0 +1,10 @@
+var options = {
+	"colorHover" : "brown",
+	"colorMap" : {
+		"Old English" : "blue",
+		"French" : "green"
+	},
+	"colorProperty" : "origin",
+	"xProperty" : "term",
+	"yProperty" : "percent",
+};
